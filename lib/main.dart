1c591cf3e157257386fbc4c778f3bfd9a8230218
Tatiana Vargas <jud.vargas@udla.edu.co>
2025-11-21 22:01:35 -0500

    Agregar Display Container: padding, decoración y boxShadow

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,13 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, //bandera de debug
       title: 'Calculadora',
       theme: ThemeData(
-        brightness: Brightness.dark,
+        brightness: Brightness.dark, // Tema oscuro para toda la app
         useMaterial3: true,
       ),
-      home: const CalculatorScreen(),
+      home: const CalculatorScreen(), // Pantalla principal
     );
   }
 }
@@ -28,10 +29,27 @@ class CalculatorScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFF1A1A1A), // Fondo oscuro
       body: const SafeArea(
-        child: Center(
-          child: Text('Calculadora', style: TextStyle(fontSize: 22),
+        child: Padding(
+          padding: EdgeInsets.all(16.0), // Separación de bordes
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity, // Ocupar todo el ancho disponible
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.grey, // Color temporal (se ajusta en commit 5)
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(24.0), // Espacio interno del display
+                    child: SizedBox(
+                      height: 80, // Altura del área donde irá el texto
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        )
+        ),
       ),
     );
   }
